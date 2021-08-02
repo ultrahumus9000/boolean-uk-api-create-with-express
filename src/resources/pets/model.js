40e0afd7a4d0068bc814bb94b4e1b9f105709996
Linlin Li <paris1900lll@gmail.com>
2021-08-02 22:09:10 +0100

    first commit

---
+++ src/resources/pets/model.js
@@ -0,0 +1,41 @@
+const db = require("../../utils/database");
+const { buildAnimalDatabase } = require("../../utils/mockData");
+
+function Pet() {
+  function createTable() {
+    const sql = `
+      CREATE TABLE IF NOT EXISTS pets (
+        id        SERIAL        PRIMARY KEY,
+        name      VARCHAR(255)   NOT NULL,
+        age       INTEGER       NOT NULL,
+        type      VARCHAR(255)   NOT NULL,
+        breed     VARCHAR(255)   NOT NULL,
+        microchip BOOLEAN       NOT NULL
+      );
+    `;
+
+    db.query(sql)
+      .then((result) => console.log("[DB] Pet table ready."))
+      .catch(console.error);
+  }
+
+  function mockData() {
+    const createPet = `
+      INSERT INTO pets
+        (name, age, type, breed, microchip)
+      VALUES
+        ($1, $2, $3, $4, $5)
+    `;
+
+    const pets = buildAnimalDatabase();
+
+    pets.forEach((pet) => {
+      db.query(createPet, Object.values(pet));
+    });
+  }
+
+  createTable();
+  mockData();
+}
+
+module.exports = Pet;
